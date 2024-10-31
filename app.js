32a116bd9220292431ba5e625e3e67ce04803e9b
Dima Serikov <ext-dmitry.serikov@flowbird.group>
2024-11-01 02:09:50 +0300

    4097 Developing form homework

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
+const path = require('path');
+const fs = require('fs');
 
 require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const hostname = process.env.HOSTNAME;
+const filePath = path.join(__dirname, 'db.json');
 
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 
+// Main page
 app.get('/', (req, res) => {
   const error = req.query.error ? decodeURIComponent(req.query.error) : null;
   const name = req.query.name || '';
@@ -25,27 +29,67 @@ app.get('/', (req, res) => {
   });
 });
 
-app.post(
-  '/submit',
-  [
-    body('name').notEmpty().withMessage('Name required').isLength({ min: 2 }).withMessage('Name must be greater or equal than 2 symbols'),
-    body('email').isEmail().withMessage('Input correct email')
-  ],
-  (req, res) => {
+
+const validationOptions = [
+  body('name').notEmpty().withMessage('Name required').isLength({ min: 2 }).withMessage('Name must be greater or equal than 2 symbols'),
+  body('email').isEmail().withMessage('Input correct email')
+];
+
+// Action 
+app.post('/submit', validationOptions, (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       const errorMessages = errors.array().map(error => error.msg).join('. ');
-      return res.redirect(`/3097_form_validation/?error=${encodeURIComponent(errorMessages)}&name=${req.body.name}&email=${req.body.email}`);
+      return res.redirect(`/?error=${encodeURIComponent(errorMessages)}&name=${req.body.name}&email=${req.body.email}`);
     }
 
-    res.render('success', {
+    addDataToFile({
       name: req.body.name,
       email: req.body.email
     });
-  }
-);
 
+    return res.redirect('/success');
+});
+
+// Result 
+app.get('/success', (req, res) => {
+
+  if (!fs.existsSync(filePath)) {
+    res.render('success', {data: []});
+  }
+  
+  const data = fs.readFileSync(filePath);
+
+  let parsedData;
+  
+  try {
+    parsedData = JSON.parse(data);
+  } catch (err) {
+    parsedData = [];
+  }
+
+  if (!Array.isArray(parsedData)) {
+    parsedData = [parsedData];
+  }
+  
+  res.render('success', {data: parsedData});
+});
+
+// Helpers
+function addDataToFile(newData) {
+  let data = [];
+
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    data = JSON.parse(fileData);
+  }
+
+  data.push(newData);
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
+}
+
+// Listen
 app.listen(port, hostname, () => {
   console.log(`Server is running on port http://localhost:${port}`)
 });
